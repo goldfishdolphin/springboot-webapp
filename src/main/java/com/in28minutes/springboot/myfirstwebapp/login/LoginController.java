84d377f9417356a48253db92ebd5b49fffd94746
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-04 11:23:24 +0000

    hard code authentocation

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -11,6 +11,13 @@ import org.slf4j.LoggerFactory;
 
 @Controller
 public class LoginController {
+	private AuthenticationService authenticationService;
+	
+
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
 
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String goToLoginPage(){
@@ -19,9 +26,12 @@ public class LoginController {
 	
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
+		if(authenticationService.authenticate(name, password)) {
 		model.put("name", name );
 		model.put("password", password );
-;		return "welcome";
+		return "welcome";
 	}
+		return "loginerror";
+		}
 	
 }

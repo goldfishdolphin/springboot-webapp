0cd5eddd04dfd2a800b0f5160c783d4b3d52a0d6
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-05 18:18:40 +0000

    create a hard coded todo list

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,10 +1,14 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
-	private static List <Todo> todos;
+	private static List <Todo> todos = new ArrayList<>();
 	static {
 		todos.add(new Todo(1, "in28minutes", "Learn Spring Boot", LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(2, "in28minutes", "Learn Cloud Computing", LocalDate.now().plusYears(2), false));

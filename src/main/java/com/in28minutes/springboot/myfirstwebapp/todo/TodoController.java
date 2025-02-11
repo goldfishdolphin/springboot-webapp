f99cd753d9da5e3538c9cf5d6972ca1cc61469f7
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-11 16:44:13 +0000

    implement validation to bean and display error message

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -6,12 +6,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -36,12 +39,15 @@ public class TodoController {
 	@RequestMapping(value = "add-todo" , method = RequestMethod.GET)
 	public String showNewTodo(ModelMap model) {
 		String username = (String)model.get("name");
-		Todo todo = new Todo(0, username,"", LocalDate.now().plusYears(1), false);
+		Todo todo = new Todo(0, username,"Add description", LocalDate.now().plusYears(1), false);
 		model.put("todo", todo);
 		return "todo";
 	}
 	@RequestMapping(value = "add-todo" , method = RequestMethod.POST)
-	public String addNewTodo(ModelMap model, Todo todo) {
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		if (result.hasErrors()) {
+			return "todo";
+		}
 		String username = (String)model.get("name");
 		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:todo-list";

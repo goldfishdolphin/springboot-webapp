b5f42d4bf7809e3c4952f07f04f91c11a146d96b
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-12 11:30:57 +0000

    delete a todo by id

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -52,5 +52,12 @@ public class TodoController {
 		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:todo-list";
 	}
+	
+	@RequestMapping("delete-todo")
+	public String deleteTodo(@RequestParam int id) {
+		todoService.deleteById(id);
+		return "redirect:todo-list";
+		}
+	
 
 }

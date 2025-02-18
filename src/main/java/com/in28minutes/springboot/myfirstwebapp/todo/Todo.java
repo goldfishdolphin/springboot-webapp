3ead6fafef0428e29ac81b56cca6c4251bb5805d
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-18 19:49:06 +0000

    populate data to h2

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -2,8 +2,13 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		super();
@@ -13,6 +18,8 @@ public class Todo {
 		this.targetDate = targetDate;
 		this.done = done;
 	}
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
 	@Size(min=10, message= "Enter at least 10 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'Admin', 'Get AWS Cerrtified', CURRENT_DATE(), false);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'Admin', 'Get Azure Cerrtified', CURRENT_DATE(), false);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'Admin', 'Get GCP Cerrtified', CURRENT_DATE(), false);
